39c9ef320fbbde1fd51c5bc4e5f1a3e9388880e7
Rajiv2809 <121161360+Rajiv2809@users.noreply.github.com>
2025-12-04 18:33:53 +0700

    update import exel

--- panitia/import.php
+++ panitia/import.php
@@ -47,7 +47,7 @@ if (isset($_FILES['file_excel']['name'])) {
 
     echo "<script>
             alert('Data berhasil diimport!');
-            window.location.href='pengguna.php';
+            window.location.href='pengguna2.php';
           </script>";
     exit;
 }
